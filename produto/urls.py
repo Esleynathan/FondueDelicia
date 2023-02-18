b6fea4904124e8b90df052bd596dcb4ef830d880
Esley <esleynathan@hotmail.com>
2023-02-18 19:36:08 -0300

    FondueDelicia | Iniciando Carrinho, URL/View add_carrinho.

--- produto/urls.py
+++ produto/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", views.home, name='home'),
     path("categoria/<int:id>", views.categorias, name='categoria'), 
     path("produto/<int:id>", views.produto, name='produto'),
+    path("add_carrinho", views.add_carrinho, name='add_carrinho'),
+    path("ver_carrinho", views.ver_carrinho, name='ver_carrinho'),
+
 ]
